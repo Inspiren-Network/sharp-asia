d4de231599c46a2afb27cee972a11c5adaf65ece
Ooi Kim Siah <kimsiahooi@gmail.com>
2024-09-11 19:56:19 +0800

    feat: added product detail quotation

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@
     <SheetTrigger asChild>
       <div
         v-if="totalProducts"
-        class="w-[50px] h-[50px] rounded-full bg-[#ea1c24] fixed bottom-[150px] right-[10px] cursor-pointer inline-flex items-center justify-center z-10"
-      >
+        class="w-[50px] h-[50px] rounded-full bg-[#ea1c24] fixed bottom-[150px] right-[10px] cursor-pointer inline-flex items-center justify-center z-10">
         <span
-          class="absolute top-[17.5%] left-1/2 bg-black text-white size-[17px] inline-flex justify-center items-center rounded-full text-[12px]"
-          >{{ totalProducts }}</span
-        >
+          class="absolute top-[17.5%] left-1/2 bg-black text-white size-[17px] inline-flex justify-center items-center rounded-full text-[12px]">
+          {{ totalProducts }}
+        </span>
         <i class="fa-solid fa-receipt text-white text-[20px]"></i>
       </div>
     </SheetTrigger>
@@ -23,8 +22,7 @@
               :product="product"
               :increaseQuantity="increaseQuantity"
               :decreaseQuantity="decreaseQuantity"
-              :removeQuote="removeQuote"
-            />
+              :removeQuote="removeQuote" />
           </SheetDescription>
         </ScrollArea>
         <div class="justify-end">
@@ -34,12 +32,9 @@
             {{ totalProducts }}
           </p>
           <SheetClose asChild>
-            <Button
-              class="popmake-1867 w-full cursor-pointer"
-              @click="handleGetQuote"
-              :disabled="totalProducts === 0"
-              >Get Quotation</Button
-            >
+            <Button class="popmake-1867 w-full cursor-pointer" @click="handleGetQuote" :disabled="totalProducts === 0">
+              Get Quotation
+            </Button>
           </SheetClose>
         </div>
       </SheetHeader>
@@ -83,10 +78,7 @@ export default {
   },
   computed: {
     totalProducts() {
-      return this.localStorageProducts.reduce(
-        (total, currentProduct) => total + currentProduct.quantity,
-        0
-      )
+      return this.localStorageProducts.reduce((total, currentProduct) => total + currentProduct.quantity, 0)
     },
   },
   methods: {
@@ -96,10 +88,7 @@ export default {
       )
       if (!checkProduct.length) {
         this.localStorageProducts.push(product)
-        localStorage.setItem(
-          'oks-vue',
-          JSON.stringify(this.localStorageProducts)
-        )
+        localStorage.setItem('oks-vue', JSON.stringify(this.localStorageProducts))
         toast('Product added to quotation.', {
           type: 'success',
           position: 'top-right',
@@ -120,16 +109,9 @@ export default {
     },
     handleGetQuote() {
       document.addEventListener('click', (event) => {
-        if (
-          event.target.matches(
-            '.forminator-custom-form .forminator-button-submit'
-          )
-        ) {
+        if (event.target.matches('.forminator-custom-form .forminator-button-submit')) {
           this.localStorageProducts = []
-          localStorage.setItem(
-            'oks-vue',
-            JSON.stringify(this.localStorageProducts)
-          )
+          localStorage.setItem('oks-vue', JSON.stringify(this.localStorageProducts))
         }
       })
     },
@@ -154,17 +136,12 @@ export default {
           return p
         })
         this.localStorageProducts = newLocalStorageProducts
-        localStorage.setItem(
-          'oks-vue',
-          JSON.stringify(this.localStorageProducts)
-        )
+        localStorage.setItem('oks-vue', JSON.stringify(this.localStorageProducts))
       }
     },
   },
   mounted() {
-    const allProducts = document.querySelectorAll(
-      '.solution-product-row .vc_row.vc_inner'
-    )
+    const allProducts = document.querySelectorAll('.solution-product-row .vc_row.vc_inner')
 
     const productCategory = document.querySelector(
       '#header ul[role="menubar"] > li[class*="current"] > ul.sub-menu > li[class*="current"] > a'
@@ -172,9 +149,7 @@ export default {
 
     allProducts.forEach((product) => {
       const productTitle = product.querySelector('.wpb_raw_code h5').innerText
-      const imageUrl = product
-        .querySelector('.wpb_single_image img')
-        .getAttribute('src')
+      const imageUrl = product.querySelector('.wpb_single_image img').getAttribute('src')
 
       const quoteButton = product.querySelector('.quotation-btn')
 
@@ -192,24 +167,42 @@ export default {
       this.products.push({ productTitle, productCategory, imageUrl })
     })
 
+    const productDetail = document.querySelectorAll('.nec-product-desc')
+
+    productDetail.forEach((product) => {
+      const productTitle = product.querySelector('.info-title').innerText
+      const imageUrl = product.querySelector('.product-image:first-child img').getAttribute('src')
+
+      const quoteButton = product.querySelector('.quotation-btn')
+
+      if (quoteButton) {
+        quoteButton.addEventListener('click', () =>
+          this.addQuote({
+            productTitle,
+            productCategory,
+            imageUrl,
+            quantity: 1,
+          })
+        )
+      }
+    })
+
     const localStorageProducts = localStorage.getItem('oks-vue')
 
     if (!localStorageProducts) {
       localStorage.setItem('oks-vue', JSON.stringify([]))
     } else {
-      const newLocalStorageProducts = JSON.parse(localStorageProducts).map(
-        (product) => {
-          if (!product.quantity) {
-            product.quantity = 1
-          }
-
-          if (!product.productCategory) {
-            product.productCategory = null
-          }
-
-          return product
+      const newLocalStorageProducts = JSON.parse(localStorageProducts).map((product) => {
+        if (!product.quantity) {
+          product.quantity = 1
         }
-      )
+
+        if (!product.productCategory) {
+          product.productCategory = null
+        }
+
+        return product
+      })
 
       this.localStorageProducts = newLocalStorageProducts
       localStorage.setItem('oks-vue', JSON.stringify(newLocalStorageProducts))
